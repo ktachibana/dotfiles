14dd743423ab3459639da61e666e46fd441f19c0
Kenichi Tachibana <deadzone@be.to>
2015-04-19 19:57:04 +0900

    rbenvの本体・プラグインのインストールとdefault-gemsを追加

---
+++ setup-rbenv.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+git clone https://github.com/sstephenson/rbenv-default-gems.git ~/.rbenv/plugins/rbenv-default-gems
+ln -s ~/dotfiles/default-gems ~/.rbenv/
